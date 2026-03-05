f2bca88db877e2dc4e9fa21723fac6f6df4bf669
Lloydgapton <lloydgapton123@gmail.com>
2026-03-05 11:30:13 +0000

    added components for the project

---
+++ src/pages/index.tsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import Navbar from "@/components/Navbar";
+import HeroSection from "@/components/HeroSection";
+
+
+const Index = () => {
+  const [waitlistOpen, setWaitlistOpen] = useState(false);
+  const [sponsorOpen, setSponsorOpen] = useState(false);
+
+  return (
+    <div className="min-h-screen bg-background">
+      <Navbar />
+      <HeroSection onCTAClick={() => setWaitlistOpen(true)} />
+    </div>
+  );
+};
+
+export default Index;
